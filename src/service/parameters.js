e77f08922a7a72898b0c792e7a0c1e6e45b6134b
Alejo <alejogarb25@gmail.com>
2021-03-20 22:20:24 -0300

    Create parameters.js

---
+++ src/service/parameters.js
@@ -0,0 +1,13 @@
+export default function getParametersfromUrl (url) {
+    let offset;
+    let limit;
+    try {
+      offset = /offset=([0-9]+)/gi.exec(url).pop();
+      limit = /limit=([0-9]+)/gi.exec(url).pop();
+    } catch (e) {
+      offset = undefined;
+      limit = undefined;
+    }
+    return { offset, limit };
+  }
+  
